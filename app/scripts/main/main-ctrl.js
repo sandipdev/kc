3c87e348f9c242a61aeaf3da8fe230a76465d018
wrseward <wrseward@gmail.com>
2014-07-21 19:24:07 -0400

    Add basic styling and update main controller logic
    Update main view to use orderBy and date filters and update controller logic accordingly

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -2,22 +2,22 @@
 
 angular.module('karmaChameleon')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/v1/posts')
-      .then( function(posts) {
-        $scope.posts = posts.data;
-      });
-    $scope.newPost = { body: '' };
+
+    $http.get('/api/v1/posts').then( function(posts) {
+      $scope.posts = posts.data;
+    });
+
     $scope.createPost = function(newPost) {
       $http.post('/api/v1/post', newPost)
-        .success( function(post) {
-          $scope.posts.push(post);
+        .then( function(post) {
+          $scope.posts.push(post.data);
+          $scope.newPost.body = '';
         });
-      $scope.newPost = { body: '' };
     };
-    $scope.deletePost = function(index, post) {
-      $http.delete('/api/v1/post/' + post._id)
-        .success( function() {
-          $scope.posts.splice(index, 1);
-        });
+
+    $scope.deletePost = function(post) {
+      var index = $scope.posts.indexOf(post);
+      $scope.posts.splice(index, 1);
+      $http.delete('/api/v1/post/' + post._id);
     };
   });
