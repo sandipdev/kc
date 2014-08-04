3e72919f5e93ba75347c814347ca133955cdca48
wrseward <wrseward@gmail.com>
2014-08-03 22:10:13 -0400

    Add e2e test to use isEnabled function and test angular moment

--- test/e2e/suggestion_test.js
+++ test/e2e/suggestion_test.js
@@ -3,35 +3,39 @@
 describe('The main view', function () {
   var textArea = element(by.model('ctrl.newSuggestion.body'));
   var submitButton = element(by.name('createSuggestionForm')).$('input[type=submit]');
-  var suggestion = element.all(by.repeater('suggestion in ctrl.suggestions')).last().element(by.binding('suggestion.body'));
+  var suggestions =  element.all(by.repeater('suggestion in ctrl.suggestions'));
+  var lastSuggestion = suggestions.last().element(by.binding('suggestion.body'));
 
   beforeEach(function () {
     browser.get('http://localhost:3000');
   });
 
-  it('should have the correct title', function () {
+  it('has the correct title', function () {
     expect(browser.getTitle()).toBe('Karma Chameleon');
   });
 
   it('allows visitors to suggest', function () {
     textArea.sendKeys('This is great!');
     submitButton.click();
-    expect(suggestion.getText()).toBe('This is great!');
+    expect(lastSuggestion.getText()).toContain('This is great!');
     expect(textArea.getAttribute('value')).toEqual('');
   });
 
-  it('does not allow blank suggestions', function () {
-    expect(submitButton.getAttribute('disabled')).toBeTruthy();
-    textArea.sendKeys('I am not sure about this');
-    expect(submitButton.getAttribute('disabled')).toBeFalsy();
-    textArea.clear();
-    expect(submitButton.getAttribute('disabled')).toBeTruthy();
+  it('displays how long ago a suggestion was made', function () {
+    textArea.sendKeys('I just made this suggestion.');
+    submitButton.click();
+    expect(lastSuggestion.$('.time-ago').getText()).toEqual('a few seconds ago');
+    expect(lastSuggestion.$('.time-ago').getAttribute('title')).toContain('Today');
   });
 
-  it('does not allow short suggestions', function () {
+  it('does not allow blank or short suggestions', function () {
+    expect(submitButton.isEnabled()).toBeFalsy();
     textArea.sendKeys('Hi there!');
-    expect(submitButton.getAttribute('disabled')).toBeTruthy();
-    textArea.sendKeys('!');
-    expect(submitButton.getAttribute('disabled')).toBeFalsy();
+    expect(submitButton.isEnabled()).toBeFalsy();
+    textArea.clear();
+    expect(submitButton.isEnabled()).toBeFalsy();
+    textArea.sendKeys('I am not sure about this');
+    expect(submitButton.isEnabled()).toBeTruthy();
   });
+
 });
