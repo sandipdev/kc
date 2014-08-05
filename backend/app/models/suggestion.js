3b3bb3e0609958d5e754ae0ec265dae77b936be4
wrseward <wrseward@gmail.com>
2014-08-05 08:06:32 -0400

    Add sessions and voting routes
    Add libraries express-session and mongo-session to limit voting server side, along with routes for voting

--- backend/app/models/suggestion.js
+++ backend/app/models/suggestion.js
@@ -10,4 +10,10 @@ var SuggestionSchema = new Schema({
   downVotes: { type: Number, min: 0, default: 0 }
 });
 
+SuggestionSchema.virtual('score').get(function () {
+  return this.upVotes - this.downVotes;
+});
+
+SuggestionSchema.set('toJSON', { virtuals: true });
+
 module.exports = mongoose.model('Suggestion', SuggestionSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 'use strict';
 
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
+var express      = require('express');
+var app          = express();
+var bodyParser   = require('body-parser');
+var mongoose     = require('mongoose');
+var session      = require('express-session');
+var MongoStore   = require('connect-mongo')(session);
 
 app.use(bodyParser.json());
+app.use(session({
+  secret: 'secret',
+  store: new MongoStore({
+    db: 'karmaChameleon'
+  }),
+  cookie: { maxAge: 6000 },
+  resave: true,
+  saveUninitialized: true
+}));
 var port = process.env.PORT || 8080;
 
 mongoose.connect('mongodb://localhost/karmaChameleon');
@@ -39,6 +50,26 @@ router.route('/suggestions')
       res.status(201).json( suggestion );
     });
   });
+
+router.route('/suggestions/:suggestionId/:voteAction(up|down)')
+  .post(function(req, res) {
+    var session = req.session;
+    var suggestionId = req.params.suggestionId;
+    var voteAction = req.params.voteAction + 'Votes';
+    var voteObj = {};
+    voteObj[voteAction] = 1;
+
+    if (session[suggestionId]) {
+      res.status(304).end();
+      return;
+    } else {
+      session[suggestionId] = req.params.voteAction;
+      Suggestion.findByIdAndUpdate(suggestionId, { $inc: voteObj }, function (err, suggestion) {
+          res.send(suggestion);
+      });
+    }
+  });
+
 
 app.use('/api/v1', router);
 
